a1a79fc2f063925ff3676d30e5c4caad405c2d9a
Kevin Charm <kevin@kevincharm.com>
2023-06-11 02:15:11 +0200

    add stuff idek

--- contracts/PonziRep.sol
+++ contracts/PonziRep.sol
@@ -4,20 +4,32 @@ pragma solidity 0.8.18;
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {ERC20, ERC20Permit, ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Sets} from "./Sets.sol";
 
 contract PonziRep is ERC20Votes {
     using Strings for address;
+    using Sets for Sets.Set;
+
+    enum TradeOfferStatus {
+        Uninitialised, /** Empty state; trade does not exist */
+        Initialised, /** Initialised and waiting for counterparty to commit */
+        Finalised, /** Done */
+        Cancelled
+    }
 
     struct TradeOffer {
         uint256 uReceive;
         uint256 iReceive;
-        bool isFinalised;
+        TradeOfferStatus status;
     }
 
     mapping(address => uint256) public userTradeNonce;
     mapping(address => mapping(uint256 => TradeOffer)) public tradeOffers;
+    Sets.Set private tradeOffersSet;
     address public governor;
     mapping(address => bool) public shunned;
+    // referree (address that got referred) -> referrer
+    mapping(address => address) public referrers;
 
     event TradeOfferCreated(
         address indexed offerer,
@@ -34,10 +46,18 @@ contract PonziRep is ERC20Votes {
     event TradeOfferWithdrawn(address indexed cryptoSide, uint256 nonce);
     event AtInverseBrah(address rekt);
 
-    constructor(string memory name_, string memory symbol_)
-        ERC20(name_, symbol_)
-        ERC20Permit(name_)
-    {}
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address[] memory fundingFathers
+    ) ERC20(name_, symbol_) ERC20Permit(name_) {
+        tradeOffersSet.init();
+        require(fundingFathers.length >= 5, "Not enough funding fathers");
+        // The funding fathers get a premine
+        for (uint256 i; i < fundingFathers.length; ++i) {
+            _mint(fundingFathers[i], 10e18);
+        }
+    }
 
     // TODO: Limit this to onlyOwner or something
     function setGovernance(address governor_) external {
@@ -57,10 +77,22 @@ contract PonziRep is ERC20Votes {
         );
     }
 
+    function invite(address noob) external {
+        // Only members with a certain threshold (let's say 10PP) can invite
+        // new members to the community. Then they become the referral that
+        // receives 50% of any social capital awarded from trades of the
+        // referee.
+        require(balanceOf(msg.sender) >= 10e18, "Not enough social capital");
+        referrers[noob] = msg.sender;
+        // Mint 1 PP
+        _mint(noob, 1e18);
+    }
+
     function createTradeOffer(uint256 uReceive, uint256 iReceive)
         external
         payable
     {
+        require(balanceOf(msg.sender) >= 1e18, "Not enough social capital");
         require(msg.value >= uReceive, "Not enough ETH deposited");
         _assertNotShunned(msg.sender);
         uint256 nonce = userTradeNonce[msg.sender]++;
@@ -68,16 +100,20 @@ contract PonziRep is ERC20Votes {
             TradeOffer({
                 uReceive: uReceive,
                 iReceive: iReceive,
-                isFinalised: false
+                status: TradeOfferStatus.Initialised
             })
         );
+        tradeOffersSet.add(keccak256(abi.encode(msg.sender, nonce)));
         emit TradeOfferCreated(msg.sender, nonce, uReceive, iReceive);
     }
 
     function withdrawTradeOffer(uint256 nonce) external {
-        TradeOffer memory tradeOffer = tradeOffers[msg.sender][nonce];
-        require(!tradeOffer.isFinalised, "Trade already finalised");
-        delete tradeOffers[msg.sender][nonce];
+        TradeOffer storage tradeOffer = tradeOffers[msg.sender][nonce];
+        require(
+            tradeOffer.status == TradeOfferStatus.Initialised,
+            "Offer not initialised"
+        );
+        tradeOffer.status = TradeOfferStatus.Cancelled;
         (bool success, ) = msg.sender.call{value: tradeOffer.uReceive}("");
         require(success, "Refund failed");
         emit TradeOfferWithdrawn(msg.sender, nonce);
@@ -111,8 +147,11 @@ contract PonziRep is ERC20Votes {
         TradeOffer storage tradeOffer = tradeOffers[cryptoSide][
             offerCreatorNonce
         ];
-        require(!tradeOffer.isFinalised, "Offer already finalised");
-        tradeOffer.isFinalised = true;
+        require(
+            tradeOffer.status == TradeOfferStatus.Initialised,
+            "Offer not initialised"
+        );
+        tradeOffer.status = TradeOfferStatus.Finalised;
         (bool success, ) = payable(fiatSide).call{value: tradeOffer.uReceive}(
             ""
         );
@@ -127,6 +166,19 @@ contract PonziRep is ERC20Votes {
         // +1 social credit (bing chilling)
         ERC20Votes._mint(cryptoSide, 1e18);
         ERC20Votes._mint(fiatSide, 1e18);
+
+        // +0.5 social credit to the referrers (next LEVEL up)
+        // NB:
+        //  - referrer might be zero address (funding fathers)
+        //  - referrer might be shunned
+        address cryptoSideReferrer = referrers[cryptoSide];
+        if (cryptoSideReferrer != address(0) && !shunned[cryptoSideReferrer]) {
+            ERC20Votes._mint(cryptoSide, 1e18 / 2);
+        }
+        address fiatSideReferrer = referrers[fiatSide];
+        if (fiatSideReferrer != address(0) && !shunned[fiatSideReferrer]) {
+            ERC20Votes._mint(fiatSide, 1e18 / 2);
+        }
     }
 
     function shun(address naughtyBoy) external onlyGovernance {
@@ -142,5 +194,25 @@ contract PonziRep is ERC20Votes {
         uint256
     ) internal virtual override {
         revert("lol no");
+    }
+
+    function getTradesCount() external view returns (uint256) {
+        return tradeOffersSet.size;
+    }
+
+    /// @notice Returns all trades regardless of their status (#yolonoindexer)
+    function getTrades() external view returns (bytes32[] memory out) {
+        uint256 size = tradeOffersSet.size;
+        if (size == 0) {
+            return new bytes32[](0);
+        }
+
+        out = new bytes32[](size);
+        bytes32 element = tradeOffersSet.tail();
+        for (uint256 i; i < size; ++i) {
+            out[size - i - 1] = element;
+            element = tradeOffersSet.prev(element);
+        }
+        return out;
     }
 }
